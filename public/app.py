a15ed63b978c4c95a6a9b42f0b2c464c48645769
ThiagoBristot <altf4nfunfa@gmail.com>
2024-09-05 12:24:40 -0300

    ajustes conforme desejo cliente

--- public/app.py
+++ public/app.py
@@ -7,15 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/images/<filename>')
-def images(filename):
-    return send_from_directory('Resources', filename)
-
-@app.route('/get_images')
-def get_images(): 
-    image_files = os.listdir('Resources')
-    images = [f'/images/{img}' for img in image_files if img.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-    return {'images': images}
-
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
--- public/script.js
+++ public/script.js
@@ -7,17 +7,24 @@ function showContent(section) {
             'Resources/20230206_162217.jpg',
             'Resources/qtw1ix.jpg'
         ];
-        let imagesHTML = '<div class="background">';'<h2>Projetos</h2><div class="image-grid"></div>';
+        
+        let imagesHTML = '<h2>Projetos</h2><div class="image-grid">';
         images.forEach((img, index) => {
             imagesHTML += `
                 <div class="image-item">
                     <img src="${img}" onclick="showOverlay('${img}')">
                 </div>`;
+            if ((index + 1) % 3 === 0) {
+                imagesHTML += '<div class="clear"></div>';
+            }
         });
         imagesHTML += '</div>';
         contentDiv.innerHTML = imagesHTML;
     } else if (section === 'sobre') {
         contentDiv.innerHTML = '<h2>Sobre Mim</h2><p>Informações sobre mim...</p>';
+        } 
+    else if (section === 'contato') {
+        contentDiv.innerHTML = '<a href="https://www.instagram.com/sua_conta/" target="_blank" id="Btn"><button onclick="showContent("contato")" class="Btn"><svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512" class="svgIcon"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg><span class="text">Instagram</span></button></a>'
         }
 }
 
@@ -37,7 +44,7 @@ function closeOverlay() {
     overlay.style.display = 'none';
 }
 
-function generateStar() {
+/*function generateStar() {
     const starsContainer = document.getElementById('stars-container');
     const star = document.createElement('div');
     star.className = 'stars';
@@ -56,4 +63,14 @@ function generateStar() {
 document.addEventListener('DOMContentLoaded', () => {
     // Gera estrelas continuamente a cada 50ms
     setInterval(generateStar, 50);
-});
+});*/
+
+window.onload = function() {
+    let images = document.querySelectorAll('.image-item img');
+    let minHeight = Math.min(...Array.from(images).map(img => img.naturalHeight));
+
+    images.forEach(img => {
+        img.style.height = minHeight + 'px';
+        img.style.width = 'auto'; // Mantém a proporção original
+    });
+};
